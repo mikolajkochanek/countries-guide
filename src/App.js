6d7097a4f355c25a2cfdf37f89df215859a3b1f0
Mikołaj Kochanek <mikolajkoch04@gmail.com>
2023-01-13 18:39:34 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes, Link} from 'react-router-dom'
+import NavBar from "./components/Header/NavBar";
+
+const navLinks= [
+  <Link to="/">Test</Link>, 
+  <Link to="/">Test</Link>,
+  <Link to="/">Test</Link>, 
+  <Link to="/">Test</Link>,
+]
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar navItems={navLinks} >
+      </NavBar>
+      <Routes>
+        <Route path="/">main page</Route>
+      </Routes>
+   
+      
+     
     </div>
   );
 }
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,26 @@
+import './NavBar.css'
+export default function NavBar({navItems, children}) {
+
+    const areChildrenPassed = !!children;
+
+    const renderedChildren = areChildrenPassed ? <div className='children-container'>{children}</div> : null;
+
+    const renderedNavItems = navItems.map((navItem,index) => {
+       return <li className='nav-item' key={index}>{navItem}</li>
+    })
+
+    return <nav> 
+        <div className='brand'>
+            <span className="brand-blue">World </span>
+            <span className="brand-red">Explorer</span>
+        </div>
+        <div className='nav-container'>
+            <ul>
+                {renderedNavItems}
+            </ul>
+        </div>
+        {renderedChildren}
+        </nav>
+
+
+}
